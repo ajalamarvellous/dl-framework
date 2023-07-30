d368a8fcf9e3b241380865f39b753e43bf12c9cc
shelby <ajalaoluwamayowa00@gmail.com>
2023-07-30 04:22:41 +0100

    Add pre-commit edits

--- dl_framework/base.py
+++ dl_framework/base.py
@@ -1,8 +1,10 @@
 """Base functions and classes will be found here"""
 import numpy as np
 
-class nn():
+
+class nn:
     """A neural network implementation in numpy"""
+
     def __init__(self, input, no_output):
         """Initialising the weights for the neural networks"""
         self._no_output = no_output
@@ -10,5 +12,5 @@ class nn():
 
     def forward(self, input):
         if not self._weights_set:
-            self._weights = np.random.randn((input.shape[1], no_output))
+            self._weights = np.random.randn((input.shape[1], self._no_output))
         return self._input @ self._weight
--- dl_framework/cli.py
+++ dl_framework/cli.py
@@ -1,13 +1,16 @@
 """Console script for dl_framework."""
 import sys
+
 import click
 
 
 @click.command()
 def main(args=None):
     """Console script for dl_framework."""
-    click.echo("Replace this message by putting your code into "
-               "dl_framework.cli.main")
+    click.echo(
+        "Replace this message by putting your code into "
+        "dl_framework.cli.main"  # noqa
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
