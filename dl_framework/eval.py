3100d39bf96beb3cb827f8155939b35efe8da36e
shelby <ajalaoluwamayowa00@gmail.com>
2023-08-02 18:38:17 +0100

    Make RMSE process list instead of individual values

--- dl_framework/eval.py
+++ dl_framework/eval.py
@@ -1,5 +1,7 @@
 """Script to hold some evaluation metrics function"""
 
+from typing import List
+
 import numpy as np
 
 
@@ -8,7 +10,11 @@ class RMSE:
         """Root mean square implementation"""
         pass
 
-    def __call__(self, target: int, value: int) -> int:
-        self.mse_ = (target - value) ** 2
-        self.rmse_ = np.sqrt(self.mse_)
-        return self.mse_
+    def __call__(self, y_true: List[float], y_pred: List[float]) -> List:
+        if len(y_true) != len(y_pred):
+            raise Exception(
+                f"The len of y_true {len(y_true)} does not match the len of y_pred{len(y_pred)}"  # noqa
+            )
+        self.mse_ = (np.array(y_true) - np.array(y_pred)) ** 2
+        self.rmse_ = np.average(np.sqrt(self.mse_))
+        return np.average(self.mse_)
