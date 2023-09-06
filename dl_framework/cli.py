ec6c6dcfb96c4bb4e959ef9b07a805cd8c2b2914
shelby <ajalaoluwamayowa00@gmail.com>
2023-09-06 09:39:44 +0100

    Refactor cli.py

--- dl_framework/cli.py
+++ dl_framework/cli.py
@@ -36,22 +36,27 @@ def main(x_data, y_data):
     relu = Relu()
     # optim = GradientDescent()
     rmse = RMSE()
-    alpha = 0.00001
+    alpha = 0.00000001
     print("Models parameters successfully set...")
 
-    for i, (X_train, Y_train) in enumerate(zip(x_train, y_train)):
-        for _ in range(300):
-            choices = np.random.choice(round(len(y_train) * 0.7), 10)
-            output = model(np.array(x_train)[choices])
-            print(f"Forward prop {i, _ +1} done....")  # result: {output}")
+    n = 0
+    y_pred = []
+    for _ in range(300):
+        for i, (X_train, Y_train) in enumerate(zip(x_train, y_train)):
+            output = model(X_train)
             output = relu(output)
-            # print(
-            # f"Correct value:{np.array(y_train)[choices]}... Output:{output}")
-            error = rmse(np.array(y_train)[choices], output)
+            n += 1
+            y_pred.append(output)
+            print(f"Forward prop {n} done....")
+            print(f"Correct value:{Y_train}... Output:{output}")
+            error = rmse(Y_train, output)
             print(f"Calculating error, Error: {error}....")
             weight_delta = (np.array(X_train) * error * alpha).reshape(-1, 1)
             model._weights -= weight_delta
             print(f"Backprop done successfully... \n {'-' * 50} \n")
+
+    error = rmse(Y_train, y_pred)
+    print(f"The RMSE of the model is {error}....done.")
 
     return
 
