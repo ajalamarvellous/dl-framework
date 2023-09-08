9d5e5f30691787936264e81af0afaf8ca56a967c
shelby <ajalaoluwamayowa00@gmail.com>
2023-09-08 04:09:02 +0100

    Edit assertion to ensure predictions and true values have the same shape

--- dl_framework/eval.py
+++ dl_framework/eval.py
@@ -10,17 +10,16 @@ class RMSE:
         """Root mean square implementation"""
         pass
 
-    def __call__(self, y_true: List[float], y_pred: List[float]) -> List:
-        if type(y_true) == list:
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> List:
+        n = 1
+        assert type(y_true) == type(
+            y_pred
+        ), f"Different type formats: y_true type {type(y_true)} y_pred {type(y_pred)}"  # noqa
+        if isinstance(type(y_true), np.ndarray):
             assert (
-                y_pred == list
-            ), "Only one prediction returned and list of true values given"  # noqa
-            assert len(y_true) != len(
-                y_pred
+                y_true.shape[0] == y_pred.shape[0]
             ), f"The len of y_true {len(y_true)} does not match the len of y_pred{len(y_pred)}"  # noqa
-            n = len(y_true)
-        else:
-            n = 1
+            n = y_true.shape[0]
         self.mse_ = (np.array(y_true) - np.array(y_pred)) ** 2
         self.rmse_ = np.sqrt(np.sum(self.mse_)) / n
         return self.rmse_
